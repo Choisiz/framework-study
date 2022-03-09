229ea32d5301885c8d3985c579669195401620ac
dnjsvltm201311681 <dnjsvltm201311681@gmail.com>
2022-03-09 23:41:25 +0900

    pages

--- pages/about.js
+++ pages/about.js
@@ -1,3 +1,8 @@
+//파일의 이름을 uri 주소로 작성해준다.
+//반면 컴포넌트의 이름은 중요하지 않다.
+//router의 기능을 가능케 해준다.
+//export default 필수
+//파일이름과 다른 uri를 입력하면 404페이지를 로드
 export default function Potato() {
   return "about";
 }
--- pages/index.js
+++ pages/index.js
@@ -1,3 +1,4 @@
+//index라는 명은 default uri로 만들어준다
 export default function Home() {
   return "hi";
 }
