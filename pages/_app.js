ba67a569145d327dcc5dede2278150858fc40bfc
dnjsvltm201311681 <dnjsvltm201311681@gmail.com>
2022-03-17 23:45:18 +0900

    복습

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,7 @@
 //무조건 이이름 이어야함
 //맨처음 랜더링 되는 페이지
 //function 이름은 상관없다.
-
+//jsx global을 사용하여 모든 글로벌하게 css를 줄수 있다.
 import NavBar from "../components/NavBar";
 
 export default function App({ Component, pageProps }) {
