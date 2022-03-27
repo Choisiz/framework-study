ead0d45afe56b76f3a42b1e09baa849042fb8512
dnjsvltm201311681 <dnjsvltm201311681@gmail.com>
2022-03-27 23:58:33 +0900

    api같은 중요정보 숨기기

--- pages/index.js
+++ pages/index.js
@@ -2,18 +2,12 @@
 import { useEffect, useState } from "react";
 import Seo from "../components/Seo";
 
-const API_KEY = "c036ad604f59edfdca963014a847889b";
-
 export default function Home() {
   const [movies, setMovies] = useState();
 
   useEffect(() => {
     (async () => {
-      const { results } = await (
-        await fetch(
-          `https://api.themoviedb.org/3/movie/popular?api_key=${API_KEY}`
-        )
-      ).json();
+      const { results } = await (await fetch(`/api/movies`)).json();
       console.log("ghgg", { results });
       setMovies(results);
     })();
